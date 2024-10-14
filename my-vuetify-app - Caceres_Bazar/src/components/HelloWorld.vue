782c426c9f7757d10a3365dca894f58aa3164929
Omar Alejandro Bazar <69990247+alejandroX86@users.noreply.github.com>
2024-10-14 20:48:59 -0300

    Add files via upload

---
+++ my-vuetify-app - Caceres_Bazar/src/components/HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+ 
+ 
+ <div class="hello">
+    <h1>{{ msg }}</h1>
+   
+   
+    <h2>Integrantes</h2>
+    <h3>Leonardo Caceres</h3>
+    <h3>Omar Bazar</h3>
+   
+  </div>
+ 
+ <div class="d-flex justify-space-around">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          Crear Tareas
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-btn
+      color="primary"
+    >
+      Seguimiento estado de Tareas
+
+      <v-menu activator="parent">
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
+
+    <v-btn
+      id="menu-activator"
+      color="primary"
+    >
+      Asignacion de Tareas
+    </v-btn>
+
+    <v-menu activator="#menu-activator">
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+    }),
+  }
+</script>
---
+++ my-vuetify-app - Caceres_Bazar/src/main.js
@@ -0,0 +1,22 @@
+//import { createApp } from 'vue'
+//import App from './App.vue'
+
+//createApp(App).mount('#app')
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'vuetify/dist/vuetify.min.css';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+
+app.use(vuetify);
+
+app.mount('#app');
